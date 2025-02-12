5b20e3d7e81254d4bc2b23edf1abc6ce0f4a491e
Deveshpardhi <150068044+Deveshpardhi@users.noreply.github.com>
2025-02-12 13:32:53 +0530

    Time: 30 ms (5.41%), Space: 45.7 MB (39.74%) - LeetHub

--- 3522-find-the-power-of-k-size-subarrays-i/3522-find-the-power-of-k-size-subarrays-i.java
+++ 3522-find-the-power-of-k-size-subarrays-i/3522-find-the-power-of-k-size-subarrays-i.java
@@ -1,35 +1,30 @@
 class Solution {
-    // Updated method: now checks that each adjacent pair differs by exactly 1.
     public static boolean isSorted(List<Integer> list) {
         for (int i = 1; i < list.size(); i++) {
-            // Instead of checking using compareTo, we subtract adjacent elements.
-            // If the difference is not exactly 1, the elements are not consecutive.
-            if (list.get(i) - list.get(i - 1) != 1) {
+            if (list.get(i)-list.get(i-1) != 1) {
                 return false;
             }
         }
         return true;
     }
-    
     public int[] resultsArray(int[] nums, int k) {
-        int arr[] =  new int[nums.length - k + 1];
-        ArrayList<Integer> list = new ArrayList<>();
-        int max = -1;
+        int arr[] =  new int[nums.length-k+1];
+        //Map<ArrayList,int> power=new HashMap<>();
+        ArrayList<Integer> list=new ArrayList<>();
+        int max=-1;
         
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = i; j < i + k; j++) {
+        for(int i=0;i<arr.length;i++){
+            for(int j=i;j<i+k;j++){
                 list.add(nums[j]);
             }
             
-            if (isSorted(list)) {
-                // Since the window is consecutive and sorted,
-                // its maximum element is Collections.max(list) (or simply the last element).
-                max = Collections.max(list);
-                arr[i] = max;
-            } else {
-                arr[i] = -1;
+            if(isSorted(list)){
+                max=Collections.max(list);
+                arr[i]=max;
+            }else{
+                arr[i]=-1;
             }
-            max = -1;
+            max=-1;
             list.clear();
         }
         return arr;
