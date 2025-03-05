56c2065d81c94fd659470b8157473a34a281cb0c
Deveshpardhi <150068044+Deveshpardhi@users.noreply.github.com>
2025-03-05 10:58:50 +0530

    Time: 0 ms (100%), Space: 40.5 MB (56.96%) - LeetHub

---
+++ 2575-minimum-cuts-to-divide-a-circle/2575-minimum-cuts-to-divide-a-circle.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int numberOfCuts(int n) {
+        int res=0;
+        if(n==1){
+            res=0;
+        }else if(n%2==0){
+            res=n/2;
+        }else{
+            res=n;
+        }
+        return res;
+    }
+}
