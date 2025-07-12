d4c14988c505ede827c8e21b5ffe32b692fc910b
Deveshpardhi <150068044+Deveshpardhi@users.noreply.github.com>
2025-07-12 20:56:58 +0530

    Time: 0 ms (100%), Space: 40.8 MB (18.06%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,17 +1,19 @@
 class Solution {
+    public int algo(int n,int arr[]){
+        if(n==0){
+            return 1;
+        }
+        if(n<0){
+            return 0;
+        }
+        if(arr[n]!=0){
+            return arr[n];
+        }
+        arr[n]=algo(n-1,arr)+algo(n-2,arr);
+        return arr[n];
+    }
     public int climbStairs(int n) {
-        if(n<=2){
-            return n;
-        }
-        int n1=1;
-        int n2=2;
-
-        for(int i=3;i<=n;i++){
-            int temp=n1+n2;
-            n1=n2;
-            n2=temp;
-        }
-
-        return n2;
+        int arr[]=new int[n+1];
+        return algo(n,arr);
     }
 }
