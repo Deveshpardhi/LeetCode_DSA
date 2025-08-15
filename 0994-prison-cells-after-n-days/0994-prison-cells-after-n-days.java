a5a1388fda7d9fd2febffc2ff967e6228a2f039b
Deveshpardhi <150068044+Deveshpardhi@users.noreply.github.com>
2025-08-15 15:02:44 +0530

    Time: 4 ms (27.5%), Space: 43.5 MB (20.83%) - LeetHub

--- 0994-prison-cells-after-n-days/0994-prison-cells-after-n-days.java
+++ 0994-prison-cells-after-n-days/0994-prison-cells-after-n-days.java
@@ -1,15 +1,32 @@
+import java.util.*;
+
 class Solution {
     public int[] prisonAfterNDays(int[] cells, int n) {
-        int cycle = 14; 
-        n = (n - 1) % cycle + 1; 
-
-        for (int day = 0; day < n; day++) {
-            int[] next = new int[8];
-            for (int i = 1; i < 7; i++) {
-                next[i] = (cells[i - 1] == cells[i + 1]) ? 1 : 0;
+        Map<String, Integer> seenDay = new HashMap<>();
+        List<int[]> order = new ArrayList<>();
+        
+        while (n > 0) {
+            String key = Arrays.toString(cells);
+            if (seenDay.containsKey(key)) {
+                int cycleStart = seenDay.get(key);
+                int cycleLen = order.size() - cycleStart;
+                n %= cycleLen; // jump ahead
+                return order.get(cycleStart + n);
+            } else {
+                seenDay.put(key, order.size());
+                order.add(cells);
             }
-            cells = next;
+            cells = nextDay(cells);
+            n--;
         }
         return cells;
+    }
+
+    private int[] nextDay(int[] a) {
+        int[] next = new int[8]; // ends default to 0
+        for (int i = 1; i < 7; i++) {
+            next[i] = (a[i - 1] == a[i + 1]) ? 1 : 0;
+        }
+        return next;
     }
 }
